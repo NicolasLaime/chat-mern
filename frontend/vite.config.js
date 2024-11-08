2886d699c36f201bcc94708cae11e594a557a511
NicolasLaime <nicolaime100@gmail..com>
2024-11-08 19:46:43 -0300

    arreglamos frontend parte 4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,9 +7,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_BACKEND_URL, 
+        target: import.meta.env.VITE_BACKEND_URL,  
         secure: false,
       },
     },
   },
-})
+});
